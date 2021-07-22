63d4c49d765d8e40b46d2b9a23fa2e61d986d90e
Saúl Moreno Abril <93sauu@gmail.com>
2021-07-22 10:48:02 +0200

    wip

--- react-tutorial/search-bar/src/App.js
+++ react-tutorial/search-bar/src/App.js
@@ -1,49 +1,59 @@
-import React from 'react';
+import React, {useCallback, useMemo, useState} from 'react';
 import './App.css';
 import { fetchSearchResults } from './utils.js';
 import ListItem from './ListItem.js';
 import SearchInput from './SearchInput.js';
 import _ from 'lodash';
 
+const useMergeState = (initialState) => {
+  const [state, setState] = useState(initialState)
 
-// what the meaning of cb
-const fetchData = async (query, cb) => {
-  const res = await fetchSearchResults(query);
-  cb(res);
-};
+  const customSetState = useCallback((newer) => {
+    setState(prev => ({...prev, ...newer}))
+  }, [setState])
 
-const debouncedFetchData = _.debounce(( query, cb) => {
-  fetchData(query, cb);
-},500 );
+  return [state, customSetState]
+}
 
 export default function App() {
-  const [query, setQuery] = React.useState('');
-  const [results, setResults] = React.useState([]);
-  
+  const [state, setState] = useMergeState({
+    loading: false,
+    query: '',
+    results: [],
+  })
+  const {loading, query, results} = state
 
-  React.useEffect(() => {
-    debouncedFetchData( query, res=> {
-      setResults(res);
-    });
-  }, [query]);
+  const fetchDrinksByText = useCallback(async (text) => {
+    setState({loading: true})
+    const results = await fetchSearchResults(text)
+    setState({results, loading: false})
+  }, [setState])
+
+  const debouncedFetchDrinksByText = useMemo(() => _.debounce(fetchDrinksByText,500), [fetchDrinksByText])
+
+  const onChangeText = useCallback((ev) => {
+    const query = ev.target.value
+    setState({query})
+    debouncedFetchDrinksByText(query)
+  }, [debouncedFetchDrinksByText, setState])
+
   return (
     <div>
-      <h1> We are a healthy web, though we do not reject beer APIs :</h1>
-    <SearchInput
-      value={query}
-      onChangeText={e => {
-      setQuery(e.target.value);
-      }}
-    />
-    {results.map((result, index) => (
-      <div key={index}>
-      <ListItem
-        title={result.name}
-        imageUrl={result.image_url}
-        caption={result.tagline}
+      <h1>  At this page, we are not alcoholic,but this free API is stunning:</h1>
+      <SearchInput
+        value={query}
+        onChangeText={onChangeText}
       />
-      </div>
-    ))}
+      {loading && <div>Loading</div>}
+      {results.map((result, index) => (
+        <div key={index}>
+        <ListItem
+          title={result.name}
+          imageUrl={result.image_url}
+          caption={result.tagline}
+        />
+        </div>
+      ))}
     </div>
-    );
+  );
 }
